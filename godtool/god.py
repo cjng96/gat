bfaffb6c52b355d5f4378e601ef0666b9b99ad6b
FelixChoi <cjng96@gmail.com>
2019-06-19 16:09:03 +0900

    god.py: use dict2 for g_config

--- godtool/god.py
+++ godtool/god.py
@@ -35,14 +35,14 @@ class MyUtil():
 	def __init__(self):
 		self.executableName = ""
 		self.deployRoot = ""	# only for deployment
-		self.deployOwner = ""	
+		self.deployOwner = None
 		self.isRestart = True	# First start or modified source files
 
 class Dict2():
 	def __init__(self, dic=None):
-		if dic is None:
-			dic = dict()
-		self.dic = dic
+		self.dic = dict()
+		if dic is not None:
+			self.fill(dic)
 
 	def __getattr__(self, name):
 		if name in self.dic:
@@ -58,16 +58,31 @@ class Dict2():
 	@staticmethod
 	def make(dic):
 		dic2 = Dict2()
+		dic2.fill(dic)
+		return dic2
+
+	def fill(self, dic):
 		for key, value in dic.items():
 			if type(value) == dict:
-				dic2.dic[key] = Dict2.make(value)
+				self.dic[key] = Dict2.make(value)
 			else:
-				dic2.dic[key] = value
-		return dic2
+				self.dic[key] = value
 		
 	def get(self, name, default):
-		if name in self.dic:
-			return self.dic[name]
+		lst = name.split("[.]")
+		dic = self.dic
+		for item in lst:
+			if type(dic) == dict:
+				if name in dic:
+					dic = dic[item]
+				else:
+					return default
+			else:
+				if name in dic.dic:
+					dic = dic.dic[item]
+				else:
+					return default
+
 		return default
 
 	def add(self, name, value):
@@ -89,7 +104,7 @@ class Tasks():
 		self.outStream = None
 
 		self.dic = Dict2()
-		self.dic.add("name", g_config["config"]["name"])
+		self.dic.add("name", g_config.config.name)
 
 		if server is None:
 			self.server = None
@@ -334,7 +349,7 @@ class MyHandler(PatternMatchingEventHandler):
 	def on_created(self, event):
 		self.process(event)
 
-g_config = {}
+g_config = Dict2()
 g_mygod = None
 
 g_util = MyUtil()
@@ -344,7 +359,7 @@ g_remote = None
 
 def configServerGet(name):
 	server = None
-	for it in g_config["servers"]:
+	for it in g_config.servers:
 		if it["name"] == name:
 			server = it
 			print("deploy: selected server - ", it)
@@ -368,7 +383,7 @@ def taskDeploy(serverName):
 	global g_remote
 	g_remote = Tasks(server)
 
-	name = g_config["config"]["name"]
+	name = g_config.config.name
 	targetPath = server["targetPath"]
 	realTarget = g_remote.run("mkdir -p %s/shared && cd %s && mkdir -p releases && pwd" % (targetPath, targetPath))
 	realTarget = realTarget.strip("\r\n")	# for sftp
@@ -378,21 +393,21 @@ def taskDeploy(serverName):
 	releases = list(filter(lambda x: re.match('\d{6}_\d{6}', x) is not None, res.split()))
 	releases.sort()
 
-	max = g_config["deploy"]["maxRelease"]-1
+	max = g_config.deploy.maxRelease-1
 	cnt = len(releases)
 	print("deploy: releases folders count is %d" % cnt)
 	if cnt > max:
 		print("deploy: remove old %d folders" % (cnt - max))
 		removeList = releases[:cnt-max]
 		for ff in removeList:
-			if g_util.deployOwner != "":
+			if g_util.deployOwner is not None:
 				g_remote.run("sudo rm -rf %s/releases/%s" % (targetPath, ff))
 			else:
 				g_remote.run("rm -rf %s/releases/%s" % (targetPath, ff))
 
 	# if deploy / owner is defined,
 	# create release folder as ssh user, upload, extract then change release folder to deploy / owner
-	if g_util.deployOwner != "":
+	if g_util.deployOwner is not None:
 		res = g_remote.run("cd %s/releases && sudo mkdir %s && sudo chown %s: %s" % (targetPath, todayName, server["id"], todayName))
 	else:
 		res = g_remote.run("cd %s/releases && mkdir %s" % (targetPath, todayName))
@@ -405,13 +420,9 @@ def taskDeploy(serverName):
 	# upload files
 	realTargetFull = os.path.join(realTarget, "releases", todayName)
 	include = []
-	exclude = []
-	sharedLinks = []
-	include = g_config["deploy"]["include"]
-	if "exclude" in g_config["deploy"]:
-		exclude = g_config["deploy"]["exclude"]
-	if "sharedLinks" in g_config["deploy"]:
-		sharedLinks = g_config["deploy"]["sharedLinks"]
+	include = g_config.deploy.include
+	exclude = g_config.get("deploy.exclude", [])
+	sharedLinks = g_config.get("deploy.sharedLinks", [])
 
 	def _filterFunc(pp):
 		pp = os.path.normpath(pp)
@@ -419,7 +430,7 @@ def taskDeploy(serverName):
 			return True
 		return False
 
-	strategy = g_config["deploy"]["strategy"]
+	strategy = g_config.deploy.strategy
 	if strategy == "zip":
 		zipPath = os.path.join(tempfile.gettempdir(), "data.zip")
 		with zipfile.ZipFile(zipPath, "w") as zipWork:
@@ -522,7 +533,7 @@ def taskDeploy(serverName):
 		g_remote.run("cd %s && mkdir -p shared/%s && ln -sf %s/shared/%s releases/%s/%s" % (targetPath, folder, targetPath, pp, todayName, pp))
 
 	# update link
-	if g_util.deployOwner != "":
+	if g_util.deployOwner is not None:
 		g_remote.run("cd %s && sudo rm current && sudo ln -sf releases/%s current && sudo chown %s: current" % (targetPath, todayName, server["id"]))
 	else:
 		g_remote.run("cd %s && rm current && ln -sf releases/%s current" % (targetPath, todayName))
@@ -531,7 +542,7 @@ def taskDeploy(serverName):
 	if hasattr(g_mygod, "deployPostTask"):
 		g_mygod.deployPostTask(util=g_util, local=g_local, remote=g_remote)
 
-	if g_util.deployOwner != "":
+	if g_util.deployOwner is not None:
 		g_remote.run("cd %s && sudo chown %s: releases/%s current" % (targetPath, g_util.deployOwner, todayName))
 
 def initSamples(type, fn):
@@ -565,8 +576,8 @@ class Helper:
 		global g_config
 		if type == "yaml":
 			try:
-				g_config = yaml.safe_load(ss)
-				g_util.executableName = g_config["config"]["name"]
+				g_config = Dict2(yaml.safe_load(ss))
+				g_util.executableName = g_config.config.name
 			except yaml.YAMLError as e:
 				raise e
 
@@ -619,6 +630,8 @@ def main():
 				print("god setup FILE.py")
 				return
 			target = sys.argv[2]
+			if not target.endswith(".py"):
+				target += ".py"
 		else:
 			print("unknown command - %s" % cmd)
 			return
@@ -629,7 +642,13 @@ def main():
 
 	# check first
 	if not os.path.exists(target):	# or not os.path.exists("god.yml"):
-		print("god-tool V%s\nThere is no god relevent file. you can run god YOUR_GOD_FILE.py or initialize by 'god init'" % __version__)
+		print("""god-tool V%s\n
+There is no %s script file.
+god init (sys|app) [GOD_FILE] - Generate god file.
+god [GOD_FILE] - Serve application service. use god_my.py file if GOD_FILE is skipped.
+god deploy SERVER_NAME - Deploy the application to the server.
+god setup [GOD_FILE] SERVER_NAME - Setup server defined in GOD_FILE.
+""" % (__version__, target))
 		return
 
 	helper = Helper()
@@ -640,16 +659,15 @@ def main():
 
 	print("god-tool V%s" % __version__)
 	global g_config
-	name = g_config["config"]["name"]
-	type = g_config["config"]["type"] if "type" in g_config["config"] else "app"
+	name = g_config.config.name
+	type = g_config.get("config.type", "app")
 
 	print("** config[type:%s, name:%s]" % (type, name))
 	global g_local
 	g_local = Tasks(None)	
 
 	if cmd == "deploy":
-		if "owner" in g_config["deploy"]:
-			g_util.deployOwner = g_config["deploy"]["owner"]
+		g_util.deployOwner = g_config.get("deploy.owner", None)
 
 		target = sys.argv[2] if cnt > 2 else None
 		if target is None:
@@ -664,6 +682,9 @@ def main():
 		return
 
 	# serve
+	if type != "app":
+		print("god [GOD_FILE] can be used for application type only.")
+		return
 	taskServe()
 
 def taskSetup(target, serverName):
@@ -687,7 +708,7 @@ def taskSetup(target, serverName):
 
 def taskServe():
 	observer = Observer()
-	observer.schedule(MyHandler(g_config["serve"]["patterns"]), path=".", recursive=True)
+	observer.schedule(MyHandler(g_config.serve.patterns), path=".", recursive=True)
 	observer.start()
 
 	g_util.isRestart = True
