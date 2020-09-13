b62a73bbb991ae992868b57ded88476aa6c77494
FelixChoi <cjng96@gmail.com>
2020-09-13 22:22:18 +0900

    coPath: indentation

--- godtool/coPath.py
+++ godtool/coPath.py
@@ -3,22 +3,22 @@ import os
 
 
 def cutpath(parent, pp):
-	if parent[-1] != "/":
-		parent += "/"		 
+  if parent[-1] != "/":
+    parent += "/"		 
 
-	return pp[len(parent):]	
+  return pp[len(parent):]	
 
 def path2folderList(pp):
-	dirs = []
-	while len(pp) >= 1:
-		dirs.append(pp)
-		pp, _  = os.path.split(pp)
-		if pp == "/":
-			break
+  dirs = []
+  while len(pp) >= 1:
+    dirs.append(pp)
+    pp, _  = os.path.split(pp)
+    if pp == "/":
+      break
 
-	return dirs
+  return dirs
 
 def path2FolderListTest():
-	print(path2folderList("/haha/a/test.txt"))
-	print(path2folderList("haha/b/test.txt"))
-	print(path2folderList("h/c/test.txt"))
+  print(path2folderList("/haha/a/test.txt"))
+  print(path2folderList("haha/b/test.txt"))
+  print(path2folderList("h/c/test.txt"))
